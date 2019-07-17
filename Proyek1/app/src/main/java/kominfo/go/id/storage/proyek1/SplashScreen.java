732bb6e97b4165eb368f6fa2e914d78135c6dfff
dhoni48 <52016714+dhoni48@users.noreply.github.com>
2019-07-17 14:54:25 +0800

    Proyek1 dan Proyek2 Ketika ikut serta di Sesi 1 (Pagi)

---
+++ Proyek1/app/src/main/java/kominfo/go/id/storage/proyek1/SplashScreen.java
@@ -0,0 +1,25 @@
+package kominfo.go.id.storage.proyek1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //membuat object anonim dari kelas Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
